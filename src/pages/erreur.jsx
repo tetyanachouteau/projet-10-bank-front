00656f7aefcca8afd9724fb37a9a14aac988c389
Tetyana <tetyana.chouteau@gmail.com>
2024-02-03 16:55:53 +0100

    Page d'erreur en CSS + image

--- src/pages/erreur.jsx
+++ src/pages/erreur.jsx
@@ -4,6 +4,7 @@ import React from "react";
 function Erreur() {
   return (
     <div className={styles.erreur}>
+      <img src="./img/404.png" alt="Erreur 404" />
       <p>Oups! La page que vous demandez n'existe pas.</p>
     </div>
   );
