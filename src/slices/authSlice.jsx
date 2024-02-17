817644b349f76470dc495e399b9307199a321c7a
Tetyana <tetyana.chouteau@gmail.com>
2024-02-17 10:41:32 +0100

    sauvegarde du slice dans le localstorage persistance navigateur

--- src/slices/authSlice.jsx
+++ src/slices/authSlice.jsx
@@ -5,20 +5,26 @@ import { createSlice } from '@reduxjs/toolkit';
 const authSlice = createSlice({
     name: 'auth', // Nom du slice
     initialState: { // État initial du slice
-        isAuthenticated: false, // L'utilisateur n'est pas authentifié par défaut
-        token: null, // Pas de jeton d'authentification par défaut
-        profile: null // Pas de profil utilisateur par défaut
-    },
+        // met à jour depuis le localstorage du navigateur
+        // on vérife la valeur si pas de valeur on met false
+        isAuthenticated: localStorage.getItem("isAuthenticated") ?? false, // L'utilisateur n'est pas authentifié par défaut
+        token: localStorage.getItem("token"), // Pas de jeton d'authentification par défaut
+        profile: localStorage.getItem("profile") // Pas de profil utilisateur par défaut
+    },//local storage
     reducers: { // Définition des reducers pour modifier l'état du slice
         login(state, action) { // Reducer pour gérer la connexion de l'utilisateur
             state.isAuthenticated = true; // Définir l'état d'authentification sur true
+            localStorage.setItem("isAuthenticated", true);
             state.token = action.payload.token; // Définir le jeton d'authentification avec celui fourni dans l'action
+            localStorage.setItem("token", action.payload.token);
             state.profile = action.payload.profile; // Définir le profil utilisateur avec celui fourni dans l'action
+            localStorage.setItem("profile", action.payload.profile);
         },
         logout(state) { // Reducer pour gérer la déconnexion de l'utilisateur
             state.isAuthenticated = false; // Définir l'état d'authentification sur false
             state.token = null; // Effacer le jeton d'authentification
             state.profile = null; // Effacer le profil utilisateur
+            localStorage.clear();
         },
     },
 });
