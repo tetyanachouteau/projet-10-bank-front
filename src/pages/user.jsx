dde2254165a321d97cc04c4a622f5554d184e1ad
Tetyana <tetyana.chouteau@gmail.com>
2024-03-08 16:16:45 +0100

    Mise à jours de form "modification", new swagguer de profil, méthodes delete

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -26,61 +26,52 @@ function User() {
     }
 
     const clickCancel = () => {
+        setLastName(profile.lastName);
+        setFirstName(profile.firstName);
         setIsEdit(false);
     }
 
     const clickSave = async (event) => {
         event.preventDefault();
 
+        // vérifie last et first name
+        let errorFN = validateInput(firstName);
+        setErrorFN(errorFN);
+        let errorLN = validateInput(lastName);
+        setErrorLN(errorLN);
+
         // Sauvegarde du formaulaire
 
-        if(firstName && lastName){
+        if (firstName && lastName && !(errorFN || errorLN)) {
             const profile = await API.setUserNewNames(token, firstName, lastName);
             dispatch(updateProfile(JSON.stringify(profile)));
+            setIsEdit(false);
         }
-
-        setIsEdit(false);
     }
 
     // Fonction pour gérer le changement de la valeur du prénom
-const changeFN = (event) => {
-    // Récupérer la valeur saisie dans l'événement
-    const value = event.target.value;
-
-    // Valider la valeur saisie
-    let error = validateInput(value);
-
-    // Mettre à jour l'état d'erreur en fonction du résultat de la validation
-    setErrorFN(error);
-
-    // Si aucune erreur de validation n'est détectée
-    if (!error) {
-        // Mettre à jour l'état du prénom avec la valeur saisie
-        setFirstName(value);
+    const changeFN = (event) => {
+        // Récupérer la valeur saisie dans l'événement
+        setFirstName(event.target.value);
     }
-}
 
 
     const changeLN = (event) => {
-        const value = event.target.value
-        let error = validateInput(value);
-        setErrorLN(error);
-        if(!error)
-            setLastName(value);
+        setLastName(event.target.value);
     }
 
     // Fonction pour valider une entrée
-const validateInput = (value) => {
-    console.log(value); // Affiche la valeur pour le débogage
-    let regex = /^[a-zA-Z]([a-zA-Z\s]*)[a-zA-Z]*$/gm; // Expression régulière pour vérifier si la valeur contient uniquement des lettres et espaces
-    if (!value)
-        return "La valeur ne peut pas être vide"; // Si la valeur est vide, renvoie un message d'erreur
-    else if (!value.trim())
-        return "La valeur ne peut pas être que des espaces"; // Si la valeur est composée uniquement d'espaces, renvoie un message d'erreur
-    else if (!regex.test(value))
-        return "La valeur ne peut contenir que les lettres"; // Si la valeur contient des caractères autres que des lettres et espaces, renvoie un message d'erreur
-    return null; // Si la valeur est valide, retourne null
-}
+    const validateInput = (value) => {
+        console.log(value); // Affiche la valeur pour le débogage
+        let regex = /^[a-zA-Z]([a-zA-Z\s]*)[a-zA-Z]*$/gm; // Expression régulière pour vérifier si la valeur contient uniquement des lettres et espaces
+        if (!value)
+            return "La valeur ne peut pas être vide"; // Si la valeur est vide, renvoie un message d'erreur
+        else if (!value.trim())
+            return "La valeur ne peut pas être que des espaces"; // Si la valeur est composée uniquement d'espaces, renvoie un message d'erreur
+        else if (!regex.test(value))
+            return "La valeur ne peut contenir que les lettres"; // Si la valeur contient des caractères autres que des lettres et espaces, renvoie un message d'erreur
+        return null; // Si la valeur est valide, retourne null
+    }
 
 
     // Retourne le contenu du composant
